20f35dc98b5862d9342f92a8d1b243185f22f760
fititnautal <fititnatural@gmail.com>
2018-01-17 21:02:21 +0100

    dodati xo

--- app/src/main/java/com/example/code/ixox/MainActivity.java
+++ app/src/main/java/com/example/code/ixox/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.code.ixox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int buttons[] = {R.id.b_1_1, R.id.b_1_2, R.id.b_1_3,
+                             R.id.b_2_1, R.id.b_2_2, R.id.b_2_3,
+                             R.id.b_3_1, R.id.b_3_2, R.id.b_3_3, }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < buttons.length; i++) {
+            Button button = findViewById(buttons[i]);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "protono" + view.)
+                }
+            });
+        }
+
     }
 }
